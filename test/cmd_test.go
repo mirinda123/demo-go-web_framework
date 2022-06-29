a168825c98bee9c31d3834bba62a6cc7e67cdba1
mirinda <995740977@qq.com>
2022-06-29 11:41:21 +0800

    unit test

---
+++ test/cmd_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"testing"
+
+	"github.com/mirinda123/mirinda-goweb/package/mirinda"
+)
+
+func hello(c *mirinda.Context) {
+	c.HttpString("helloworld")
+}
+func TestGET(t *testing.T) {
+	m := mirinda.New()
+
+	m.GET("/hello", hello)
+	m.ServerStart(":9999")
+
+	resp, _ := http.Get("http://localhost:9999/hello")
+	body, _ := ioutil.ReadAll(resp.Body)
+	if string(body) != "helloworld" {
+		t.Errorf("WRONG")
+	}
+
+}
